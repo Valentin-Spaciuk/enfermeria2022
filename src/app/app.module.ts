eeaf2234b21fb5ce5ba5dd1789e367395b4c8474
Valentin-Spaciuk <valenspaciuk@gmail.com>
2022-12-01 20:32:51 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PreinscripcionComponent } from './preinscripcion/preinscripcion.component';
+import { NovedadesComponent } from './novedades/novedades.component';
+import { QuienessomosComponent } from './quienessomos/quienessomos.component';
+import { CarrerasComponent } from './carreras/carreras.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { CarrouselComponent } from './carrousel/carrousel.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    QuienessomosComponent,
+    PreinscripcionComponent,
+    NovedadesComponent,
+    CarrerasComponent,
+    ToolbarComponent,
+    FooterComponent,
+    CarrouselComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
